9ea8a2e02333d795a65889cb9daf010d398188b5
melasq <masgh003@ucr.edu>
2021-06-30 14:43:29 -0700

    added test cases for each lab

--- p_I/setup_phase_1.sh
+++ p_I/setup_phase_1.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+flex mini_l.lex
+gcc -o lexer lex.yy.c -lfl
+./a.out
+cat fibonacci.min | lexer
--- p_III/setup_phase_3.sh
+++ p_III/setup_phase_3.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+mil_run mil_code.mil
+mil_run mil_code.mil < input.txt 
+# mil_run
+cat fibonacci.min | my_compiler
+echo 5 > input.txt
+mil_run fibonacci.mil < input.txt
