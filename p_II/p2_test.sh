099be756b20829b28134e6b4f33433d538440621
melasq <masgh003@ucr.edu>
2021-07-02 14:04:02 -0700

    add another test case

--- p_II/p2_test.sh
+++ p_II/p2_test.sh
@@ -5,3 +5,5 @@ flex mini_l.lex
 # sudo gcc -o parser y.tab.c lex.yy.c -ll -v
 gcc -o parser y.tab.c lex.yy.c -lfl
 cat fibonacci.min | parser
+# output cat fibonacci.min processed through parser into a specific output.txt
+# cat fibonacci.min | parser < fibonacci_output.txt
