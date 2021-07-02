2dd73be0d9b9cc28589e164d4e327d0aedeec77c
melasq <masgh003@ucr.edu>
2021-07-02 13:46:16 -0700

    Update p1_test.sh

--- p_I/p1_test.sh
+++ p_I/p1_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 flex mini_l.lex
 gcc -o lexer lex.yy.c -lfl
+# For OSX use:
+# sudo gcc -o lexer lex.yy.c -ll
+# add -v, sudo bc of my horrible threat modeling permission issues
 ./a.out
 cat fibonacci.min | lexer
