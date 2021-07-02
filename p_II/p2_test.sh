528d99bb6717fb71c9d9c77c5117754cb6918c60
melasq <masgh003@ucr.edu>
2021-07-02 14:22:01 -0700

    modeled off phase 2's website for testing output token cases

--- p_II/p2_test.sh
+++ p_II/p2_test.sh
@@ -5,5 +5,6 @@ flex mini_l.lex
 # sudo gcc -o parser y.tab.c lex.yy.c -ll -v
 gcc -o parser y.tab.c lex.yy.c -lfl
 cat fibonacci.min | parser
-# output cat fibonacci.min processed through parser into a specific output.txt
-# cat fibonacci.min | parser < fibonacci_output.txt
+# output cat fibonacci.min processed through parser into a specific output like shown in the project phase page on the website
+# cat fibonacci.min | parser < fibonacci.parse
+# cat fibonacci.parse
